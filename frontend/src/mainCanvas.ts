91176315b595e35e2c6de71d40f8a6402f57661e
zhokya <92230314+zhokya@users.noreply.github.com>
2026-05-06 12:26:08 +0200

    minimap

--- frontend/src/mainCanvas.ts
+++ frontend/src/mainCanvas.ts
@@ -364,5 +364,32 @@ export function mainCanvasRenderLoop(t: number) {
         // }
     }
 
+    if(selfBeetle !== undefined) {
+        ctx.strokeStyle = 'black';
+        ctx.fillStyle = 'rgba(0,0,0,.3)'
+        ctx.lineWidth = 2;
+
+        const minimapSize = 40;
+        const minimapMargin = 50;
+        const dotSize = 4;
+
+        ctx.beginPath();
+        ctx.arc(w - minimapMargin, minimapMargin, minimapSize, 0, Math.PI * 2);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.arc(w - minimapMargin, minimapMargin, minimapSize, 0, Math.PI * 2);
+        ctx.fill();
+
+        ctx.fillStyle = 'red';
+        ctx.beginPath();
+        ctx.arc(
+            w - minimapMargin + selfBeetle.x.value / parseInt(import.meta.env.VITE_MAP_SIZE) * (minimapSize - dotSize), 
+            minimapMargin + selfBeetle.y.value / parseInt(import.meta.env.VITE_MAP_SIZE) * (minimapSize - dotSize),
+            dotSize, 0, Math.PI * 2
+        );
+        ctx.fill();
+    }
+
     prevT = t;
 }
