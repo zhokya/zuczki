d2a06ee857d5387854c5bcfcf365771f9e8dc0f6
zhokya <92230314+zhokya@users.noreply.github.com>
2026-06-01 18:49:17 +0200

    Update minimap.ts

--- frontend/src/visuals/minimap.ts
+++ frontend/src/visuals/minimap.ts
@@ -1,5 +1,6 @@
 import { colors } from "../../../shared/looks";
 import type { LocalBeetle } from "../entities/beetle";
+import { mapSize } from "../mainCanvas";
 import { chosenLooks } from "../menu";
 import type { RenderInfo } from "../types";
 
@@ -24,13 +25,24 @@ export function renderMinimap(renderInfo: RenderInfo, selfBeetle: LocalBeetle) {
     ctx.arc(x, y, minimapSize, 0, Math.PI * 2);
     ctx.fill();
 
+    const defaultMapSize = import.meta.env.VITE_MAP_SIZE;
+    const currentMapSize = mapSize.value;
+    // (((currentMapSize - selfBeetle.size.value) / (defaultMapSize - selfBeetle.size.value) * (minimapSize - dotSize))) + dotSize = minimapSize - redEdgeSize
+    // redEdgeSize = minimapSize - dotSize - (currentMapSize - selfBeetle.size.value) / (defaultMapSize - selfBeetle.size.value) * (minimapSize - dotSize)
+    const redEdgeSize = minimapSize - dotSize - (currentMapSize - selfBeetle.size.value) / (defaultMapSize - selfBeetle.size.value) * (minimapSize - dotSize);
+    ctx.strokeStyle = 'rgba(220,50,70,' + (Math.min(1, redEdgeSize / 5) * 0.7) + ')';
+    ctx.lineWidth = redEdgeSize;
+    ctx.beginPath();
+    ctx.arc(x, y, minimapSize - redEdgeSize / 2, 0, Math.PI * 2);
+    ctx.stroke();
+
     ctx.fillStyle = colors[chosenLooks.mainColor];
     ctx.shadowBlur = 4;
     ctx.shadowColor = 'rgb(0,0,0)';
     ctx.beginPath();
     ctx.arc(
-        x + selfBeetle.x.value / import.meta.env.VITE_MAP_SIZE * (minimapSize - dotSize),
-        y + selfBeetle.y.value / import.meta.env.VITE_MAP_SIZE * (minimapSize - dotSize),
+        x + selfBeetle.x.value / (defaultMapSize - selfBeetle.size.value) * (minimapSize - dotSize),
+        y + selfBeetle.y.value / (defaultMapSize - selfBeetle.size.value) * (minimapSize - dotSize),
         dotSize, 0, Math.PI * 2
     );
     ctx.fill();
